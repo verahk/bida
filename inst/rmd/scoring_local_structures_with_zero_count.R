86277c1f4e7f7fd895a0f745ea3b00a159557363
verahk <verahk@uio.no>
2024-08-26 14:55:01 +0200

    add note on scoring partitions with zero counts

--- inst/rmd/scoring_local_structures_with_zero_count.R
+++ inst/rmd/scoring_local_structures_with_zero_count.R
@@ -1,22 +1,41 @@
 
 
-r <- 3
-q <- 11
-x <- c(.5, 1:10)
+r <- 2
+alpha <- c(1/seq(10, 1), 2, 3)
+plot(alpha, lgamma(r*alpha), type = "l")
+lines(alpha, r*lgamma(alpha), col = "red")
 
-plot(x, 2*lgamma(x))
-lines(x, lgamma(2*x))
+levels <- list(0:1, 0:1, 0:1)
+conf   <- expand.grid(levels)
+stride <- c(1, cumprod(lengths(levels[-length(levels)])))
+r <- 2
+q <- nrow(conf)
+m <- cbind(c(rep(0, 4), rep(10, 4)), c(rep(0, 4), c(10, 10, 100, 100)))
 
-counts <- matrix(x, q, r)
+# no partitioning
+tab <- m
+scores <- famscore_bdeu_byrow(tab, 1, r, q, s = 1)
+cbind(tab, scores, sum(scores))
 
-score1 <- bida:::famscore_bdeu_byrow(counts, 1, r, q, s = 1)
-score2 <- bida:::famscore_bdeu_byrow(counts, 1, r, q, s = 2)
-plot(x, score1)
-lines(x, score2, col = "red")
+# collapse two rows
+for (i in seq_along(levels)) {
+  group <- as.matrix(conf[, -i])%*%stride[-i]
+  tab <- rowsum(m, group)
+  scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(match(group, unique(group))))
+  print(cbind(tab, scores, sum(scores)))
+}
 
-# two rows with zero counts
-s <- 2
-m <- matrix(0, s, r)
-score_rows <- bida:::famscore_bdeu_byrow(m, 1, r, q, s = 1)
-bida:::famscore_bdeu_byrow(matrix(0, 1, r), 1, r, q, s = s)
-score_collapsed <- bida:::famscore_bdeu_byrow(matrix(colSums(m), 1), 1, r, q, s = s)
+# fully collapsed
+group <- rep(0, each = q)
+tab <- rowsum(m, group)
+scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(group+1))
+cbind(tab, scores, sum(scores))
+
+# split on one variable
+for (i in seq_along(levels)) {
+  group <- conf[, i]
+  tab <- rowsum(m, group)
+  scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(group+1))
+  print(cbind(tab, scores, sum(scores)))
+}
+
