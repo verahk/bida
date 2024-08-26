3ea0a5428b5a8b55c90d75e3700406415317dd89
verahk <verahk@uio.no>
2024-08-26 14:56:06 +0200

    add note on scoring partitions with zero counts

--- inst/rmd/scoring_local_structures_with_zero_count.R
+++ inst/rmd/scoring_local_structures_with_zero_count.R
@@ -17,25 +17,27 @@ tab <- m
 scores <- famscore_bdeu_byrow(tab, 1, r, q, s = 1)
 cbind(tab, scores, sum(scores))
 
-# collapse two rows
-for (i in seq_along(levels)) {
-  group <- as.matrix(conf[, -i])%*%stride[-i]
-  tab <- rowsum(m, group)
-  scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(match(group, unique(group))))
-  print(cbind(tab, scores, sum(scores)))
-}
-
 # fully collapsed
 group <- rep(0, each = q)
 tab <- rowsum(m, group)
 scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(group+1))
 cbind(tab, scores, sum(scores))
 
+
 # split on one variable
 for (i in seq_along(levels)) {
   group <- conf[, i]
   tab <- rowsum(m, group)
   scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(group+1))
+  print(cbind(tab, scores, sum(scores)))
+}
+
+
+# collapse two rows
+for (i in seq_along(levels)) {
+  group <- as.matrix(conf[, -i])%*%stride[-i]
+  tab <- rowsum(m, group)
+  scores <- famscore_bdeu_byrow(tab, 1, r, q, s = tabulate(match(group, unique(group))))
   print(cbind(tab, scores, sum(scores)))
 }
 
