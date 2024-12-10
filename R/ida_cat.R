10e56984c50f0fd867e6ae89e5d5c9c08d32ebe9
verahk <verahk@uio.no>
2024-12-10 16:23:48 +0100

    add ida routine adapted to new code

---
+++ R/ida_cat.R
@@ -0,0 +1,137 @@
+
+#' Title
+#'
+#' @param cpdag (integer matrix) an adjacency matrix representing a CPDAG, where
+#'
+#' - `cpdag[i, j] = 1` indicates an edge from node `i` to node `j`,
+#' - `cpdag[i, j] = 1` indicates absence of an edge from node `i` to node `j`,
+#' - `cpdag[i, j] = cpdag[j, i] = 1` indicates an undirected edge from node `i` to node `j`.
+#' @param data (integer matrix) data matrix representing categorical variables. Assumed to be zero-based.
+#' @param adjset (character) which adjustment set to use in backdoor formula.
+#' @param params (list) list with hyperparameters for the local bdeu-priors.
+#' @param x,y (integer vector) position of cause and effect variables, respectively.
+#' @param contrasts (list) causal contrast, see [posterior_mean.bida_posterior()].
+#' @param verbose (logical) print output to follow progression.
+#'
+#' @return a list with the following objects:
+#' - `ipts`: a matrix list with all pairwise intervention probability tables (IPTs).
+#'    This is the average over the unique set of such tables.
+#' - `tau`: a matrix list with averaged causal effects.
+#' @export
+#'
+#' @examples
+#'
+#' data(bida_example_cat)
+#' attach(bida_example_cat)
+#'
+#' # infer a CPDAG with pcalg::pc
+#' suffStat <- list(dm = data,
+#'                  nlev = apply(data, 2, max)+1,
+#'                  adaptDF = FALSE)
+#' fit <- pcalg::pc(suffStat,
+#'                  pcalg::disCItest,
+#'                  alpha = .05,
+#'                  labels = colnames(data),
+#'                  u2pd = c("retry"),
+#'                  verbose = FALSE)
+#' cpdag <- as(fit@graph, "matrix")
+#' # Rgraphviz::plot(fit@graph)
+#' cpdag
+#'
+#' # estimate intervention probabilities
+#' params <- list(ess = 1, nlev = apply(data, 2, max)+1)
+#' res <- ida_cat(cpdag, data, "pa", params, contrasts = list(jsd = jsd))
+#' res
+#'
+#' # optimal-IDA, set effects to zero exactly
+#' res <- ida_cat(cpdag, data, "o", params, contrasts = list(jsd = jsd))
+#' res
+ida_cat <- function(cpdag,
+                data,
+                adjset = c("pa", "o"),
+                params,
+                x = 1:ncol(data),
+                y = 1:ncol(data),
+                contrasts = NULL,
+                verbose = FALSE) {
+
+  adjset <- match.arg(adjset, c("pa", "o"))
+  mode(data) <- mode(params$nlev) <- "integer"
+
+  n <- ncol(data)
+  ipts <- tau <- matrix(list(), n, n)
+  for (xx in x) {
+    if (verbose) cat("Estimating unique intervention distributions for cause", xx, "\n")
+    tmp <- orient_siblings_in_cpdag(cpdag, xx)
+    if (adjset == "pa") {
+      ps <- list(tmp$parents, tmp$p)
+      posteriors <- bida_posterior_cat(ps, data, xx, y, ess = params$ess, nlev = params$nlev)
+    } else {
+      dag_supp <- dag_support(tmp$pdags)
+      posteriors <- vector("list", ncol(data))
+      for (yy in y[-match(xx, y)]) {
+        ps <- adjset_support(dag_supp, xx, yy, adjset)
+        posteriors[[yy]] <- bida_posterior_cat(ps, data, xx, yy, ess = params$ess, nlev = params$nlev)[[yy]]
+      }
+    }
+
+    ipts[xx, ] <- lapply(posteriors, posterior_mean)
+    if (length(contrasts) > 0) tau[xx, ]  <- lapply(posteriors, posterior_mean, contrasts = contrasts)
+  }
+  list(ipts = ipts, tau = tau)
+}
+
+orient_siblings_in_cpdag <- function(cpdag, x){
+
+  stopifnot(pcalg::isValidGraph(t(cpdag), type = "cpdag"))
+
+  # find siblings of x
+  posspa  <- which(cpdag[, x] == 1)
+  indx <- cpdag[x, posspa] == 1
+  sib  <- posspa[indx]              # subset with undirected edges into x
+  pa   <- posspa[!indx]             # subset with directed edges into x
+  nsib <- length(sib)
+
+  if (nsib == 0){
+    parents  <- list(pa)
+    pdags <- list(cpdag)
+  } else {
+    ## list all subsets of siblings = possible additional parents
+    if (length(sib) == 1) {
+      subsib <- list(vector("integer", 0), sib)
+      parents <- pdags <- vector("list", 2)
+    } else {
+      subsib  <- unlist(lapply(c(0, seq_along(sib)), combn, x = sib, simplify = FALSE), recursive = FALSE)
+      parents <- pdags <- vector("list", length(subsib))
+    }
+
+    # make graphNEL-object for pcalg::addBgKnowledge
+    colnames(cpdag) <- rownames(cpdag) <- seq_len(ncol(cpdag))
+    gcpdag <- as(cpdag, "graphNEL")
+
+    isInvalid <- vector("logical", length = length(subsib))
+    for (g in seq_along(subsib)){
+      # direct edges of subset s of siblings
+      s    <- subsib[[g]]
+      ns   <- length(s)
+      from <- c(s, rep(x, nsib-ns))
+      to   <- c(rep(x, ns), setdiff(sib, s))
+      gprime <- pcalg::addBgKnowledge(gcpdag, from, to)
+      if (is.null(gprime)) {
+        isInvalid[g] <- TRUE
+      } else {
+        parents[[g]]  <- c(s, pa)
+        pdags[[g]] <- as(gprime, "matrix")
+      }
+    }
+    if (all(isInvalid)){
+      stop("Edges can not be directed to a valid PDAG")
+    }
+
+    # remove list elements for invalid parent sets
+    parents[isInvalid] <- NULL
+    pdags[isInvalid] <- NULL
+  }
+  return(list(pdags = pdags, parents  = parents, p = rep(1/(1+nsib), 1+nsib)))
+}
+
